3c16ba6f926dbf36ac4d6f1b9f80ed6a760fe48c
michael <mikedy7@gmail.com>
2022-09-16 20:33:00 -0700

    .

--- packages/interface/src/pages/address-profiler/[address].tsx
+++ packages/interface/src/pages/address-profiler/[address].tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import dynamic from 'next/dynamic'
 import { Theme } from '@mui/material'
 import { withRouter } from 'next/router'
 import Box from '@mui/material/Box'
 import Card from 'components/Card'
 import ThresholdChart from 'components/ThresholdChart'
-import DataGrid from 'components/DataGrid'
 import V1Layout from 'layout/V1Layout'
+const DataGrid = dynamic(() => import('components/DataGrid'), { ssr: true })
 
 export default withRouter<any>(({ router }) => {
   const { address } = router.query
