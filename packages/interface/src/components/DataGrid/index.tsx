1a2886bce31e1c7d4e9e3d9f4ddae23a13158377
mike dy <mikedy7@gmail.com>
2022-09-20 12:56:39 -0700

    .

--- packages/interface/src/components/DataGrid/index.tsx
+++ packages/interface/src/components/DataGrid/index.tsx
@@ -68,67 +68,67 @@ const rows = [
   { id: 30, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ]
 
-const StripedDataGrid = styled(MuiDataGrid)(({ theme }) => ({
-  border: 'none',
-  '& .MuiDataGrid-columnHeaders': {
-    backgroundColor: '#0c243e',
-    borderBottom: 'none',
-    boxShadow: '0px 10px 10px 5px rgb(0 0 0 / 15%)',
-    zIndex: '10',
-  },
-  '& .MuiDataGrid-cell': {
-    borderBottom: 'none',
-  },
-  '& .MuiDataGrid-footerContainer': {
-    borderTop: 'none',
-  },
-  '& .MuiDataGrid-columnSeparator': {
-    visibility: 'hidden',
-  },
-  '& .super-app-theme--header': {
-    backgroundColor: 'red',
-  },
-  '& ::-webkit-scrollbar': {
-    width: '8px',
-  },
-  '& ::-webkit-scrollbar-track': {
-    backgroundColor: 'transparent',
-  },
-  '& ::-webkit-scrollbar-thumb': {
-    backgroundColor: 'rgb(89, 106, 118)',
-    outline: 'none',
-    borderRadius: '4px',
-  },
-  [`& .${gridClasses.row}.odd`]: {
-    backgroundColor: '#0c243e',
-    '&:hover, &.Mui-hovered': {
-      backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY),
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-    '&.Mui-selected': {
-      backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY + theme.palette.action.selectedOpacity),
-      '&:hover, &.Mui-hovered': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          ODD_OPACITY + theme.palette.action.selectedOpacity + theme.palette.action.hoverOpacity
-        ),
-        // Reset on touch devices, it doesn't add specificity
-        '@media (hover: none)': {
-          backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY + theme.palette.action.selectedOpacity),
-        },
-      },
-    },
-  },
-}))
+// const StripedDataGrid = styled(MuiDataGrid)(({ theme }) => ({
+//   border: 'none',
+//   '& .MuiDataGrid-columnHeaders': {
+//     backgroundColor: '#0c243e',
+//     borderBottom: 'none',
+//     boxShadow: '0px 10px 10px 5px rgb(0 0 0 / 15%)',
+//     zIndex: '10',
+//   },
+//   '& .MuiDataGrid-cell': {
+//     borderBottom: 'none',
+//   },
+//   '& .MuiDataGrid-footerContainer': {
+//     borderTop: 'none',
+//   },
+//   '& .MuiDataGrid-columnSeparator': {
+//     visibility: 'hidden',
+//   },
+//   '& .super-app-theme--header': {
+//     backgroundColor: 'red',
+//   },
+//   '& ::-webkit-scrollbar': {
+//     width: '8px',
+//   },
+//   '& ::-webkit-scrollbar-track': {
+//     backgroundColor: 'transparent',
+//   },
+//   '& ::-webkit-scrollbar-thumb': {
+//     backgroundColor: 'rgb(89, 106, 118)',
+//     outline: 'none',
+//     borderRadius: '4px',
+//   },
+//   [`& .${gridClasses.row}.odd`]: {
+//     backgroundColor: '#0c243e',
+//     '&:hover, &.Mui-hovered': {
+//       backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY),
+//       '@media (hover: none)': {
+//         backgroundColor: 'transparent',
+//       },
+//     },
+//     '&.Mui-selected': {
+//       backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY + theme.palette.action.selectedOpacity),
+//       '&:hover, &.Mui-hovered': {
+//         backgroundColor: alpha(
+//           theme.palette.primary.main,
+//           ODD_OPACITY + theme.palette.action.selectedOpacity + theme.palette.action.hoverOpacity
+//         ),
+//         // Reset on touch devices, it doesn't add specificity
+//         '@media (hover: none)': {
+//           backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY + theme.palette.action.selectedOpacity),
+//         },
+//       },
+//     },
+//   },
+// }))
 
 function DataGrid() {
   return (
     <Box sx={{ height: 800, width: '100%' }}>
       <DataGridPro
         rows={rows}
-        loading={rows.length === 0}
+        loading={true}
         columns={columns}
         pageSize={20}
         rowsPerPageOptions={[20]}
