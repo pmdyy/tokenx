e0b9a553f2b2b0fd393e6d861beea11241e72d4e
mike dy <mikedy7@gmail.com>
2022-09-21 10:52:42 -0700

    .

--- packages/interface/next.config.js
+++ packages/interface/next.config.js
@@ -21,7 +21,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withwithBundleAnalyzer(
+module.exports = withBundleAnalyzer(
   withPreact(
   withFonts({
     webpack(config, { dev }) {
