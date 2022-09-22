4236b938a857f7195e98b8726fe0f07310f0dfa3
michael <mikedy7@gmail.com>
2022-09-21 21:50:26 -0700

    .

--- interface/next.config.js
+++ interface/next.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const loaderUtils = require('loader-utils')
 const withFonts = require('next-fonts')
-const withPreact = require('next-plugin-preact');
+const withPreact = require('next-plugin-preact')
 
 const hashOnlyIdent = (context, _, exportName) =>
   loaderUtils
@@ -21,5 +21,21 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withPreact(
-)
+module.exports = withFonts({
+    webpack(config, { dev, isServer }) {
+      const rules = config.module.rules
+        .find((rule) => typeof rule.oneOf === 'object')
+        .oneOf.filter((rule) => Array.isArray(rule.use))
+
+      if (!dev)
+        rules.forEach((rule) => {
+          rule.use.forEach((moduleLoader) => {
+            if (moduleLoader.loader?.includes('css-loader') && !moduleLoader.loader?.includes('postcss-loader'))
+              moduleLoader.options.modules.getLocalIdent = hashOnlyIdent
+          })
+        })
+
+      return config
+    },
+    optimizeFonts: true,
+  })
