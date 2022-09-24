4f71ce9432e642b4fe7c0593a5704667f32a9d06
michael <mikedy7@gmail.com>
2022-09-24 10:42:50 -0700

    .

--- interface/src/layout/BaseLayout/index.tsx
+++ interface/src/layout/BaseLayout/index.tsx
@@ -1,8 +1,11 @@
 import React, { ReactNode, useState } from 'react'
-import Box from '@mui/material/Box'
+import dynamic from 'next/dynamic'
+// import Box from '@mui/material/Box'
+// import Header from 'components/Header'
+const Box = dynamic(() => import('@mui/material/Box'), { ssr: true })
+const Header = dynamic(() => import('components/Header'), { ssr: true })
 import CollapsibleSidebar from 'components/Sidebar'
 import Container from '@mui/material/Container'
-import Header from 'components/Header'
 import Stack from '@mui/material/Stack'
 import styled from '@emotion/styled'
 import { CSSTransition } from 'react-transition-group'
