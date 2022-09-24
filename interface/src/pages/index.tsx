084053d8c5e711127f170d7cc3086f5584489b47
michael <mikedy7@gmail.com>
2022-09-24 11:14:25 -0700

    .

--- interface/src/pages/index.tsx
+++ interface/src/pages/index.tsx
@@ -1,14 +1,9 @@
-import * as React from 'react'
+import React from 'react'
 import type { NextPage } from 'next'
-import { Container, Typography, Stack, Button } from '@mui/material'
 import { getLayout as getBaseLayout, IBaseLayout } from 'layout/BaseLayout'
 
 const Index: NextPage & IBaseLayout = () => {
-  return (
-    <Container maxWidth="lg" sx={{ marginTop: { xs: 5, md: 10 } }}>
-      <p>HomePage</p>
-    </Container>
-  )
+  return <p>HomePage</p>
 }
 
 Index.getLayout = getBaseLayout
